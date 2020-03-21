14139df8125f6effe22807795ae23b70eb0a0e42
Artyom ILYIN <artyom_ilyin@mail.ru>
2020-03-21 22:56:50 +0300

    Project updating is optimal, Assignee updating is optimal as well. Time to work out Task model updating.

--- app/models.py
+++ app/models.py
@@ -7,59 +7,88 @@ class WithAPI:
         self.api = AsanaAPI()
 
 
-class AssigneeManager(models.Manager, WithAPI):
+class AsanaModel(models.Model, WithAPI):
+    EQ_IGNORED_FIELDS = ['_state', 'api']
+
+    def __eq__(self, other):
+        values = [(k,v) for k,v in self.__dict__.items() if k not in self.EQ_IGNORED_FIELDS]
+        other_values = [(k,v) for k,v in other.__dict__.items() if k not in self.EQ_IGNORED_FIELDS]
+        return values == other_values
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.name}'
+
+    class Meta:
+        abstract = True
+
+
+class AsanaManager(models.Manager, WithAPI):
+    def create_or_update_if_necessary(self, item, model=None):
+        if not model:
+            model = self.model
+        modified = False
+        item_to_compare = model(**item)
+        item_gid = item.pop('gid')
+        model_object, created = model.objects.get_or_create(gid=item_gid, defaults=item)
+        if created or item_to_compare != model_object:
+            print(f"{model.__name__}: {item_gid} {item} object didn't exist or differs from existing. Saving...")
+            for i in item:
+                setattr(model_object, i, item[i])
+            model_object.save()
+            modified = True
+        return model_object, modified
+
+
+class AssigneeManager(AsanaManager):
     def all(self):
         users = self.api.get_all_users()
-        for row in users:
-            model_object = self.model(**row)
-            model_object.save()
+        for user in users:
+            self.create_or_update_if_necessary(user)
         return self.get_queryset()
 
 
-class ProjectManager(models.Manager, WithAPI):
+class ProjectManager(AsanaManager):
     def all(self):
         projects = self.api.get_all_projects()
-        for row in projects:
-            model_object = self.model(**row)
-            model_object.save()
-        return self.get_queryset()
+        for project in projects:
+            self.create_or_update_if_necessary(project)
+        return self.get_queryset()    
 
 
-class TaskManager(models.Manager, WithAPI):
+class TaskManager(AsanaManager):
     def all(self):
         tasks = self.api.get_all_tasks()
         foreignkey_fields = ['assignee', 'projects']
         for row in tasks:
-            object_to_save = {key: value for key, value in row.items() if key not in foreignkey_fields}
-            model_object, model_object_created = self.model.objects.get_or_create(**object_to_save)
+            initial_dict = {key: value for key, value in row.items() if key not in foreignkey_fields}
+            model_object, created = self.model.objects.get_or_create(**initial_dict)
+            if created:
+                model_object.save()
             if row['assignee']:
-                assignee, created = Assignee.objects.get_or_create(**row['assignee'])
-                if created:
-                    assignee.save()
-                model_object.assignee = assignee
-            model_object.save(from_django=False)
+                model_object.assignee, modified = self.create_or_update_if_necessary(row['assignee'], Assignee)
+                if modified:
+                    model_object.save()
             if row['projects']:
-                model_object.projects.clear()
+                updated_projects = []
                 for project in row['projects']:
-                    project, created = Project.objects.get_or_create(**project)
-                    if created:
-                        project.save()
-                    model_object.projects.add(project)
-            model_object.save(from_django=False)
+                    project_object, _ = self.create_or_update_if_necessary(project, Project)
+                    updated_projects.append(project_object)
+                if model_object.projects != updated_projects:
+                    model_object.projects.set(updated_projects)
+            if created:
+                model_object.save(from_django=False)
         return self.get_queryset()
 
 
-class Assignee(models.Model, WithAPI):
+class Assignee(AsanaModel):
     gid = models.CharField(max_length=250, primary_key=True, editable=False)
     name = models.CharField(max_length=250)
     
     objects = AssigneeManager()
 
-    def __str__(self):
-        return f'Assignee: {self.name}'
 
 
-class Project(models.Model, WithAPI):
+class Project(AsanaModel):
     gid = models.CharField(max_length=250, primary_key=True, editable=False)
     name = models.CharField(max_length=250)
 
@@ -67,9 +96,13 @@ class Project(models.Model, WithAPI):
 
     def __str__(self):
         return f'Project: {self.name}'
+    
+    def save(self, *args, **kwargs):
+        print(f"[DB]Project {self.name} saving...")
+        super(Project, self).save(*args, **kwargs)
 
 
-class Task(models.Model, WithAPI):
+class Task(AsanaModel):
     gid = models.CharField(max_length=250, primary_key=True, editable=False)
     name = models.CharField(max_length=1000)
     notes = models.TextField()
@@ -78,6 +111,10 @@ class Task(models.Model, WithAPI):
 
     objects = TaskManager()
 
+    def __init__(self, *args, **kwargs):
+        super(Task, self).__init__(*args, **kwargs)
+        self.EQ_IGNORED_FIELDS.extend(['assignee', 'projects'])
+
     def __str__(self):
         return f'Task: {self.name}'
 
@@ -85,9 +122,8 @@ class Task(models.Model, WithAPI):
         if from_django:
             update_fields = ['name', 'notes']
             update_object = {key: value for key, value in self.__dict__.items() if key in update_fields}
-            print("TASK:UPDATE:")
             projects = self.projects
-            print(update_object)
             print(projects)
             self.api.client.tasks.update_task(self.gid, params=update_object)
+        print(f"[DB]Task {self.name} saving...")
         super(Task, self).save(*args, **kwargs)
